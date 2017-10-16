bfa5705052ea66dca4d711c206eec02df0c79b93
BCshirdavani <beau.shirdavani@bellevuecollege.edu>
2017-10-15 17:02:11 -0700

    Initial Commit

---
+++ airlinedb_Xcode/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  airlinedb_Xcode
+//
+//  Created by shy macbook on 10/15/17.
+//  Copyright © 2017 shy macbook. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
